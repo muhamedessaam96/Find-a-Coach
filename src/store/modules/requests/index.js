77bfa73d6ba3b1f40c0d32a47dc1552e7515efa8
Muhamed <muhamedessam96@hotmail.com>
2021-09-26 14:00:35 +0200

    - Outputting Incoming Requests(message)

--- src/store/modules/requests/index.js
+++ src/store/modules/requests/index.js
@@ -1,5 +1,6 @@
 import mutations from './mutations.js';
 import actions from './actions.js';
+import getters from './getters.js';
 export default {
   namespaced: true,
   state() {
@@ -8,5 +9,6 @@ export default {
     };
   },
   mutations,
-  actions
+  actions,
+  getters
 };
