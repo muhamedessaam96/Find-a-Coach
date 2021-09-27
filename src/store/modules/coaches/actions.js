7b79ee727480a962ca05eab2937c857c3e0ad9c4
Muhamed <muhamedessam96@hotmail.com>
2021-09-27 12:57:23 +0200

    - Fetching Coach Data (GET Http Request)

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -17,7 +17,7 @@ export default {
       }
     );
 
-    // const responseDAta = await response.json();
+    // const responseData = await response.json();
 
     if (!response.ok) {
       // erorr ...
@@ -27,5 +27,31 @@ export default {
       ...coachData,
       id: userId
     });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://vue-http-demo-1dc13-default-rtdb.firebaseio.com/coaches.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      // ...
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas
+      };
+
+      coaches.push(coach);
+    }
+    context.commit('setCoaches', coaches);
   }
 };
