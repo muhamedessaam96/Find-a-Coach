8a51830a19c0421a4ed762c2336eb8daaf636945
Muhamed <muhamedessam96@hotmail.com>
2021-09-29 13:23:10 +0200

    Sending Coaching Requests Http Requests

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,10 +1,14 @@
 <template>
+  <base-dialog :show="!!error" title="An error occurred!" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <base-card>
       <header>
         <h2>Requests Received</h2>
       </header>
-      <ul v-if="hasRequests">
+      <base-spinner v-if="isLoading"></base-spinner>
+      <ul v-else-if="hasRequests && !isLoading">
         <request-item
           v-for="req in receivedRequests"
           :key="req.id"
@@ -19,14 +23,39 @@
 
 <script>
 import RequestItem from '../../components/requests/RequestItem.vue';
+import BaseSpinner from '../../components/ui/BaseSpinner.vue';
 export default {
-  components: { RequestItem },
+  components: { RequestItem, BaseSpinner },
+  data() {
+    return {
+      isLoading: false,
+      error: null
+    };
+  },
   computed: {
     receivedRequests() {
       return this.$store.getters['requests/requests'];
     },
     hasRequests() {
       return this.$store.getters['requests/hasRequests'];
+    }
+  },
+  created() {
+    this.loadRequests();
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (error) {
+        this.error = error.message || 'somethings failed!';
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
     }
   }
 };
--- src/store/modules/requests/mutations.js
+++ src/store/modules/requests/mutations.js
@@ -1,5 +1,8 @@
 export default {
   addRequest(statet, payload) {
     statet.requests.push(payload);
+  },
+  setRequest(state, payload) {
+    state.requests = payload;
   }
 };
